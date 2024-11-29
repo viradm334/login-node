8bcf438d09bb646beba53ba83d7d3bb111cea96c
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2024-11-29 23:25:23 +0700

    modif vercel json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const connectDB = require('../db'); 
-const authRoutes = require('../routes/auth');
+const connectDB = require('./db'); 
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const port = 3000;
